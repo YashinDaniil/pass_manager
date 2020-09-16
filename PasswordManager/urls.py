a85343c476a06a85d5a2e3977d302691adc098dc
YashinDaniil <yashinde@sovcombank.ru>
2020-09-16 16:42:13 +0300

    Update urls.py

--- PasswordManager/urls.py
+++ PasswordManager/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework_jwt.views import obtain_jwt_token
 from django.conf.urls import url
-
+from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 
 
 urlpatterns = [

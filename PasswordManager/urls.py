810f041c53ba2fc68b7064048bb8316112fe4f5c
YashinDaniil <yashinde@sovcombank.ru>
2020-09-16 16:37:56 +0300

    Update urls.py

--- PasswordManager/urls.py
+++ PasswordManager/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_jwt.views import obtain_jwt_token
+from django.conf.urls import url
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),

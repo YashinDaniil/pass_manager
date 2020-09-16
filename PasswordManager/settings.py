44ecfe4b1d7997d7e19e05e55affd8ee3a820c5d
YashinDaniil <yashinde@sovcombank.ru>
2020-09-16 17:27:24 +0300

    Update settings.py

--- PasswordManager/settings.py
+++ PasswordManager/settings.py
@@ -5,7 +5,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'password-manager-ya.herokuapp.com/*']
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost",
